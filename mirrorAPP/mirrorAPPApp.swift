a3a8733a33174144544edf4461c29d7ebe3165b0
adilmustafayilmaz <adil.yilmaz@std.antalya.edu.tr>
2026-02-12 00:45:50 +0300

    Initial Commit

---
+++ mirrorAPP/mirrorAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  mirrorAPPApp.swift
+//  mirrorAPP
+//
+//  Created by adilmustafayilmaz on 12.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct mirrorAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
